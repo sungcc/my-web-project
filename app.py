6490e67eb3cf184a5b94ec7b7a455f945ef9a912
sungcc <66457776+sungcc@users.noreply.github.com>
2023-08-10 09:20:10 +0000

    render dynamic data and add bootstrap framework

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS =[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Kwun Tong, Hongkong',
+    'salary': 'HKD. 100,000'
+  },
+  {
+    'id': 2,
+    'title': 'Programmer',
+    'location': 'New York, USA',
+    'salary': 'HKD. 200,000'
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Bangkok, Thai',
+    'salary': 'HKD. 300,000'
+  },
+  {
+    'id': 3,
+    'title': 'Backbend Engineer',
+    'location': 'New York, USA',
+    'salary': 'HKD. 400,000'
+  }
+]
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS, company_name="Sung Limited")
 
 
 if __name__ == "__main__":
